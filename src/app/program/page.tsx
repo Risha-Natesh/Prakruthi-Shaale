9db2c2cf71c36af5c5ab4196423474550dd3fb5e
RISHA N B Tech 23 <rishan.btech23@rvu.edu.in>
2025-12-16 09:40:19 +0000

    can the slide be more faster

--- src/app/program/page.tsx
+++ src/app/program/page.tsx
@@ -248,7 +248,7 @@ export default function ProgramPage() {
             <Carousel
               plugins={[
                 Autoplay({
-                  delay: 5000,
+                  delay: 3000,
                   stopOnInteraction: true,
                 }),
               ]}
